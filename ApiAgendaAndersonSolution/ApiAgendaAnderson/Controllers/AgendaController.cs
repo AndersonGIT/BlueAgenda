ef71eadf971c60274c9e94907deb7929e0f59f60
Anderson Diego José de Oliveira <anderson.jose@inovaipda.org.br>
2021-08-22 20:31:00 -0300

    Adicionando Endpoint Agenda. ++Adicionando Actions, Inserir, Atualizar, Remover, Listar e Consultar. ++Obs:  Actions adicionadas com retorno padrão.

---
+++ ApiAgendaAndersonSolution/ApiAgendaAnderson/Controllers/AgendaController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiAgendaAnderson.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class AgendaController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Consultar(long pId)
+        {
+            return Ok(new { Status = "OK", Mensagem = "Consultado com sucesso." });
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(new { Status = "OK", Mensagem = "Listado com sucesso." });
+        }
+
+        [HttpPost]
+        public IActionResult Inserir(string pEntrada)
+        {
+            return Ok(new { Status = "OK", Mensagem = "Inserido com sucesso." });
+        }
+
+        [HttpPut]
+        public IActionResult Atualizar(string pEntrada)
+        {
+            return Ok(new { Status = "OK", Mensagem = "Atualizado com sucesso." });
+        }
+
+        [HttpDelete]
+        public IActionResult Remover(long pId)
+        {
+            return Ok(new { Status = "OK", Mensagem = "Removido com sucesso." });
+        }
+    }
+}
